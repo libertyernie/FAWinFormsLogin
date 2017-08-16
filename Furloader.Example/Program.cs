079ee089b3d2fea921d759e9307210160ee4336a
libertyernie <maybeway36@gmail.com>
2017-08-16 18:19:07 -0500

    Change project to a class library and add an example project

--- Furloader.Example/Program.cs
+++ Furloader.Example/Program.cs
@@ -5,16 +5,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Furloader
-{
-    static class Program
-    {
+namespace Furloader.Example {
+    static class Program {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
-        {
+        static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             using (var f = new LoginFormFA()) {
@@ -22,7 +19,6 @@ namespace Furloader
                     MessageBox.Show(f.Cookies);
                 }
             }
-            
         }
     }
 }
